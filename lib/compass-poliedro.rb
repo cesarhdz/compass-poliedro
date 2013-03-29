5176ff87936874bf43c649885814df5e531c99a5
César Hernández <acer.mrshortcuts@gmail.com>
2013-03-28 23:48:21 -0600

    Compass Poliedro initial commit, gitignore added

---
+++ lib/compass-poliedro.rb
@@ -0,0 +1,7 @@
+require "compass-poliedro/version"
+
+module Compass
+  module Poliedro
+    # Your code goes here...
+  end
+end
---
+++ lib/compass-poliedro/version.rb
@@ -0,0 +1,5 @@
+module Compass
+  module Poliedro
+    VERSION = "0.0.1"
+  end
+end
