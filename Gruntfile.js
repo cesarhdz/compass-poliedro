ca19e9feab63fbec537a405b775a6524960bf4c4
cesarhdz <mrshortcuts@gmail.com>
2014-04-18 20:12:31 -0500

    grunt gh-pages added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,17 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // Project configuration.
   grunt.initConfig({
+    // Github-pages
+    'gh-pages': {
+      options: {
+        base: 'docs'
+      },
+      src: ['**']
+    },
     watch: {
       theme: {
         files: [
